8c028e30b287abc0b1626a6f9c05df776563992f
Brandon Ropell <brandon.ropell@gmail.com>
2022-09-13 01:52:34 -0400

    Updated R script and added images for summary stats

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,3 +5,12 @@ mecha_car_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFacto
 
 linear_regression <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_car_table)
 summary(linear_regression)
+
+
+# Deliverable 2
+suspension_coil_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+
+total_summary <- summarize(suspension_coil_table, Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+lot_summary <- summarize((group_by(suspension_coil_table, Manufacturing_Lot)),  Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
