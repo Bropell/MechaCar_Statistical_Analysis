a04dd11b8487f97481a1faa6f9e0268093185a80
Brandon Ropell <brandon.ropell@gmail.com>
2022-09-14 20:45:03 -0400

    Added deliverable 3 to R script and associated images

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -14,3 +14,16 @@ suspension_coil_table <- read.csv(file='Suspension_Coil.csv',check.names=F,strin
 total_summary <- summarize(suspension_coil_table, Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
 
 lot_summary <- summarize((group_by(suspension_coil_table, Manufacturing_Lot)),  Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+
+# Deliverable 3
+t.test(suspension_coil_table$PSI, mu=1500)
+
+Lot_1 <- subset(suspension_coil_table, Manufacturing_Lot =="Lot1")
+t.test(Lot_1$PSI, mu=1500)
+
+Lot_2 <- subset(suspension_coil_table, Manufacturing_Lot =="Lot2")
+t.test(Lot_2$PSI, mu=1500)
+
+Lot_3 <- subset(suspension_coil_table, Manufacturing_Lot =="Lot3")
+t.test(Lot_3$PSI, mu=1500)
